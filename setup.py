10412ebb8cde157402aa6623c568ab492a36bca6
David Baumgold <david@davidbaumgold.com>
2013-11-06 13:29:56 -0500

    release 0.1.6

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup
 
 setup(
     name='flask-heroku',
-    version='0.1.5',
+    version='0.1.6',
     url='https://github.com/singingwolfboy/flask-heroku',
     license='BSD',
     author='Kenneth Reitz',
